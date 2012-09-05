40a11ff78aa2d1d087ae2e468c5687bd6718d61f
dominique-bourcier <dominique.bourcier@accenture.com>
2012-09-05 15:42:51 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Emmaus::Application.config.secret_token = '40b76fa99f45001b73fdd084c9d41516227e86cc0b1edb9d58021a0dc4847199fd754d8b9b972353ff035d92afc6cc9dc60ab8be56736413f6c7568b4512ed4a'
