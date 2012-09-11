c608180edceb79219371cec5a26d514a2a97497d
dominique-bourcier <vagrant@precise32.(none)>
2012-09-11 15:32:57 +0000

    beurk

--- db/migrate/20120907122234_add_default_user_to_users.rb
+++ db/migrate/20120907122234_add_default_user_to_users.rb
@@ -1,5 +1,5 @@
 class AddDefaultUserToUsers < ActiveRecord::Migration
   def up
-    User.create(login: "admin@emmaus.org", password: "Accenture1", password_confirmation: "Accenture1")
+    #User.create(login: "admin@emmaus.org", password: "Accenture1", password_confirmation: "Accenture1")
   end
 end
